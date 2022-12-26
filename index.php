29495ee6af1339387a407b98ef6a2debd92fbb22
BegemotIPryaniki <121058375+BegemotIPryaniki@users.noreply.github.com>
2022-12-26 19:57:40 +0300

    Update index.php

--- index.php
+++ index.php
@@ -7,9 +7,13 @@
     // Mail: andrewa@nicos.ru       //
     // **************************** //
 
+    // For security
     define('__MAIN_FILE__', true);
+
+    // Modules dir
     define('__INCLUDE_PATH__', 'inc');
 
+    // Search modules in directory by mask
     function getModulesList() {
         $array = array_diff(scandir(__INCLUDE_PATH__), array('.', '..'));
         foreach($array as $elem) {
@@ -23,10 +27,12 @@
     return $array;
     }
 
+    // Include all found modules
     foreach(getModulesList() as $module) {
         include(__INCLUDE_PATH__.'/'.$module);
     }
-
+    
+    // Calling a function by name
     function func_Call($fn){
         if(is_callable($fn))
             call_user_func($fn);
@@ -34,6 +40,7 @@
             echo 'Fuction ['.$fn.'] is corrupted!';
     }
 
+    // Show menu 
     foreach($modparam as $mod) {
         foreach ($mod['func'] as $key => $value) {
             $filters[$key] = $value;
@@ -43,6 +50,7 @@
 
     echo $menu;
 
+    // Selection Handler
     foreach($filters as $key => $value) {
         if($key === $_GET["fn"]){
             func_Call($value);
